af67b60d6f9fa21fd9e1ac9c66aed5e31a523458
bk911 <bk911@DESKTOP-N6KHDCO>
2023-04-17 20:25:14 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько здесь человек?");
+            for (int amount = Convert.ToInt32(Console.ReadLine()); amount != 0; amount--)
+            {
+                Console.WriteLine(amount);
+                Console.WriteLine("Привет, как тебя зовут?");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Привет,{name}.А ты любишь читать? д/н/очл/нез");
+                string answer = Console.ReadLine();
+                if (answer == "д" | answer == "очл")
+                {
+                    Console.WriteLine("Хорошо! Книга - источник знаний.");
+                }
+                if (answer == "н" | answer == "нез")
+                {
+                    Console.WriteLine("Плохо, из книг можно узнать много интересного");
+                }
+                while (answer != "н" && answer != "д" && answer != "очл" && answer != "нез")
+                {
+                    Console.WriteLine($"{name}, ты ответил на вопрос некорректно");
+                    answer = Console.ReadLine();
+                    if (answer == "д" | answer == "очл")
+                    {
+                        Console.WriteLine("Хорошо! Книга - источник знаний.");
+                    }
+                    if (answer == "н" | answer == "нез")
+                    {
+                        Console.WriteLine("Плохо, из книг можно узнать много интересного");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
